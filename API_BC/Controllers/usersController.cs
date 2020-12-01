90b017a2d2efde097fead779135ff2a9397ad142
jor13 <ochoarj13@hotmail.com>
2020-12-01 16:09:51 -0700

    Add project files.

---
+++ API_BC/Controllers/usersController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_BC.Models;
+
+namespace API_BC.Controllers
+{
+    public class usersController : ApiController
+    {
+        private bcEntities db = new bcEntities();
+
+        // GET: api/users
+        public object Getusers()
+        {
+            return Json(db.users);
+        }
+
+        // GET: api/users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Getuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [Route("login")]
+        public bool PostuserLog(user user)
+        {
+            bool logueado = false;
+            user = db.users.Where(u => u.email.Equals(user.email) && u.password.Equals(user.password)).FirstOrDefault();
+            if (user != null)
+            {
+                logueado = true;
+            }
+            return logueado;
+        }
+
+        // PUT: api/users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putuser(int id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/users
+        [ResponseType(typeof(user))]
+        [Route("")]
+        public IHttpActionResult Postuser(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (userExists(user.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.id }, user);
+        }
+
+        // DELETE: api/users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Deleteuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private bool userExists(int id)
+        {
+            return db.users.Count(e => e.id == id) > 0;
+        }
+    }
+}
